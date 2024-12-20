235a50d732f8b19163b6cc632c04b74ff9bd619c
Jonas Wolff <90569390+JonasW01ff@users.noreply.github.com>
2024-12-20 06:39:19 +0100

    Update settings.h

--- settings.h
+++ settings.h
@@ -3,11 +3,5 @@
 #include <cmath>
 typedef float num;
 constexpr num numEPS = std::numeric_limits<num>::epsilon();
-static num numSQRTEPS = sqrt(numEPS); // Made a mistake here but I have to live with it
-static num numSQRTSQRTEPS = sqrt(sqrt(numEPS)); // Made a mistake here but I have to live with it
-/*
-typedef float num;
-constexpr num numEPS = std::numeric_limits<num>::epsilon();
-constexpr num numSQRTEPS = 1e-4; // Made a mistake here but I have to live with it
-constexpr num numSQRTSQRTEPS = 1e-2; // Made a mistake here but I have to live with it
- */
+static num numSQRTEPS = sqrt(numEPS); 
+static num numSQRTSQRTEPS = sqrt(sqrt(numEPS)); 
